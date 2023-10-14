ebde0284705245ab15731b144ed7cc77c060182f
benny <bennycortese@gmail.com>
2023-10-13 20:47:45 -0400

    Added first chunk of youtube download stuff (early commit to fight over merge conflicts and win)

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -7,3 +7,14 @@ export async function generateImage(prompt: string) {
   const resp = await fetch("https://api.quotable.io/random");
   return resp.body!.toString();
 }
+
+export async function downloadYoutubeLink(url: string) {
+  const fs = require('fs');
+  const ytdl = require('ytdl-core');
+// TypeScript: import ytdl from 'ytdl-core'; with --esModuleInterop
+// TypeScript: import * as ytdl from 'ytdl-core'; with --allowSyntheticDefaultImports
+// TypeScript: import ytdl = require('ytdl-core'); with neither of the above
+
+ytdl(url)
+  .pipe(fs.createWriteStream('video.mp4'));
+}
