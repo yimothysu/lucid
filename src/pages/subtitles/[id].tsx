d018b29e71fb8e48f5000d03c691a8433f95b723
benny <bennycortese@gmail.com>
2023-10-14 01:07:48 -0400

    Added proper state for having subtitles, and render when they are finished loading.

--- src/pages/subtitles/[id].tsx
+++ src/pages/subtitles/[id].tsx
@@ -1,18 +1,13 @@
 'use client'
-import {useEffect} from "react"
+import {useEffect, useState} from "react"
 import { useParams } from "next/navigation";
 
 export default function Subtitles () {
     
     const params = useParams()
+    
+    const [subtitles, setSubtitles] = useState(null);
 
-    if (!params || Array.isArray(params)) {
-        return (
-            <div>
-
-            </div>
-        )
-    }
 
     useEffect(() => {
         const fetchSubtitles = async (videoID = 'FSmCaprEcQM', lang = 'en') => {
@@ -23,6 +18,7 @@ export default function Subtitles () {
             console.log("Here!")
             const data = await response.json();
             console.log(data);
+            setSubtitles(data);
             // setSubtitles(data.subtitles);
             // setVideoDetails(data.videoDetails);
           } catch (error) {
@@ -35,7 +31,15 @@ export default function Subtitles () {
 
     return (
         <div>
-            So What is a man
+            {subtitles ? (
+        <div>
+          <h1>Subtitles</h1>
+          <p>Subtitle 1: {JSON.stringify(subtitles)}</p>
+          {/* Render other properties from data */}
+        </div>
+      ) : (
+        <p>Loading subtitles...</p>
+      )}
         </div>
     )
 }
