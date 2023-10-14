d91f4ac875352de3832c7d35c4f4db24d293f84a
jimsu <jimsu@protonmail.com>
2023-10-14 07:42:05 -0400

    Add timestamps and style

--- src/pages/videos/[id].tsx
+++ src/pages/videos/[id].tsx
@@ -1,12 +1,14 @@
 import { useParams } from "next/navigation";
-import { useEffect, useState } from "react";
-import YouTube from "react-youtube";
+import { useState } from "react";
+import ReactPlayer from "react-player";
 import styles from "./video.module.css";
 import {
   addVideoQuestion,
   getVideoQuestionAnswer,
 } from "@/app/firebase/firestore";
 import "@/app/globals.css";
+
+const PROGRESS_INTERVAL_MS = 500;
 
 interface QuestionAndAnswerProps {
   question: string;
@@ -24,27 +26,71 @@ function QuestionAndAnswer(props: QuestionAndAnswerProps) {
   );
 }
 
+interface ProgressBarProps {
+  progress: number;
+  duration: number;
+  onTimestampClick: (timestamp: Timestamp) => void;
+  setProgress: (progress: number) => void;
+}
+
+interface Timestamp {
+  timestamp: number;
+  question: string;
+  answer: string;
+}
+
+function ProgressBar(props: ProgressBarProps) {
+  const [timestamps, setTimestamps] = useState<Timestamp[]>([
+    {
+      timestamp: 100,
+      question: "What is the meaning of life?",
+      answer: "42",
+    },
+  ]);
+
+  return (
+    <div
+      className={styles.progressBar}
+      onClick={(e: React.MouseEvent) => {
+        const progress =
+          ((e.clientX - e.currentTarget.getBoundingClientRect().left) /
+            e.currentTarget.clientWidth) *
+          props.duration;
+        props.setProgress(progress);
+      }}
+    >
+      <div
+        className={styles.progressBar__elapsed}
+        style={{ width: (props.progress / props.duration) * 100 + "%" }}
+      ></div>
+      {timestamps.map((timestamp) => (
+        <div
+          className={styles.timestamp}
+          style={{
+            left: (timestamp.timestamp / props.duration) * 100 + "%",
+          }}
+          onClick={(e: any) => {
+            props.onTimestampClick(timestamp);
+            e.stopPropagation();
+          }}
+        ></div>
+      ))}
+    </div>
+  );
+}
+
 export default function Video() {
   const [player, setPlayer] = useState<any>(null);
-  const [elapsedTime, setElapsedTime] = useState(0);
+  const [elapsedTime, setElapsedTime] = useState<number>(0);
+  const [duration, setDuration] = useState<number>(0);
   const [question, setQuestion] = useState<string>("Question");
   const [answer, setAnswer] = useState<string>("Answer");
 
+  const [currentQuestion, setCurrentQuestion] = useState<string>("");
+  const [currentAnswer, setCurrentAnswer] = useState<string>("");
+
   const [questionResp, setQuestionResp] = useState<string[]>([]);
   const [answerResp, setAnswerResp] = useState<string[]>([]);
-
-  useEffect(() => {
-    let timer: any;
-    // player.getPlayerState() === 1 means the video is playing
-    if (player && player.getPlayerState() === 1) {
-      // Poll to get the elapsed time every second
-      timer = setInterval(() => {
-        const currentTime = Math.floor(player.getCurrentTime());
-        setElapsedTime(currentTime);
-      }, 1000);
-    }
-    return () => clearInterval(timer);
-  }, [player]);
 
   const params = useParams();
 
@@ -57,14 +103,6 @@ export default function Video() {
   }
 
   const { id } = params;
-  const handleStateChange = (event: any) => {
-    // Store the player object for future use
-    setPlayer(event.target);
-
-    // Get the elapsed time and update state
-    const currentTime = Math.floor(event.target.getCurrentTime());
-    setElapsedTime(currentTime);
-  };
 
   const onSubmit = (e: any) => {
     e.preventDefault();
@@ -83,27 +121,68 @@ export default function Video() {
     });
   }
 
+  const onReady = (player: any) => {
+    setDuration(player.getDuration());
+    setPlayer(player);
+  };
+
+  const onProgress = (progress: any) => {
+    setElapsedTime(Math.floor(progress.playedSeconds));
+  };
+
+  const onTimestampClick = (timestamp: Timestamp) => {
+    setCurrentQuestion(timestamp.question);
+    setCurrentAnswer(timestamp.answer);
+  };
+
+  const setProgress = (time: number) => {
+    player.seekTo(time);
+  };
+
   return (
-    <main>
-      <YouTube videoId={id} onStateChange={handleStateChange} />
-      <form className={styles.answerForm} onSubmit={onSubmit}>
-        <label>
+    <main className={styles.main}>
+      <div className={styles.topPart}>
+        <div className={styles.videoSection}>
+          <ReactPlayer
+            url={`https://www.youtube.com/watch?v=${id}`}
+            progressInterval={PROGRESS_INTERVAL_MS}
+            onProgress={onProgress}
+            onReady={onReady}
+            width="100%"
+          />
+          <ProgressBar
+            progress={elapsedTime}
+            duration={duration}
+            onTimestampClick={onTimestampClick}
+            setProgress={setProgress}
+          />
+        </div>
+        <div className={styles.currentQuestionSection}>
+          {currentQuestion && currentAnswer ? (
+            <>
+              <div className={styles.currentQuestion}>Current Question</div>
+              <div className={styles.currentQuestionText}>
+                {currentQuestion}
+              </div>
+              <div className={styles.currentAnswer}>Current Answer</div>
+              <div className={styles.currentAnswerText}>{currentAnswer}</div>
+            </>
+          ) : (
+            <div>Click on the progress bar to see questions and answers</div>
+          )}
+        </div>
+      </div>
+      <form className={styles.questionForm} onSubmit={onSubmit}>
+        <label htmlFor="question" className={styles.questionLabel}>
           Question
-          <input
-            type="text"
-            value={question}
-            onChange={(e) => setQuestion(e.target.value)}
-          />
         </label>
-        <label>
-          Answer
-          <input
-            type="text"
-            value={answer}
-            onChange={(e) => setAnswer(e.target.value)}
-          />
-        </label>
-        <input type="submit" value="Submit" />
+        <textarea
+          id="question"
+          value={question}
+          onChange={(e) => setQuestion(e.target.value)}
+          className={styles.questionInput}
+        />
+        <input type="submit" value="Submit" className={styles.questionSubmit} />
       </form>
       <input
         type="button"
