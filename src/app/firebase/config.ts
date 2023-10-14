9a83799595f5bde2f8b5b6dd4383edac33629347
jimsu <jimsu@protonmail.com>
2023-10-13 23:04:13 -0400

    firebase

---
+++ src/app/firebase/config.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCjjBSHWNjB6vJ46CwgwI9-OuSlL1ZYBKE",
+  authDomain: "lucid-uf.firebaseapp.com",
+  projectId: "lucid-uf",
+  storageBucket: "lucid-uf.appspot.com",
+  messagingSenderId: "1014770559790",
+  appId: "1:1014770559790:web:15e588c3393a94f3a228bf",
+};
+
+export const app = initializeApp(firebaseConfig);
