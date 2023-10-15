bed385afa5ee21f3c23a3ede715f72f9cef58b3d
jmho <justinho@ufl.edu>
2023-10-15 17:02:05 -0400

    AHHH

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -7,10 +7,10 @@ export default function Navbar(props: {
 }) {
   return (
     <div className={styles.navbar}>
-      <a className={styles.navbar__logo} href="/">
+      <Link className={styles.navbar__logo} href="/">
         <Image src="/logo-v0.png" alt="logo" width={80} height={80} />
         Lucid
-      </a>
+      </Link>
       {props.actionUrl && (
         <div className={styles.navbar__links}>
           <Link href={props.actionUrl}>{props.actionTitle}</Link>
