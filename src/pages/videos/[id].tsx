ed54c68d35e9d0f6f7ea8297dca98f5c9c259ce0
jmho <justinho@ufl.edu>
2023-10-15 03:12:55 -0400

    It works! Button for sound is integrated, now just need to clean UI (dis is benny on Justin computer for posterity)

--- src/pages/videos/[id].tsx
+++ src/pages/videos/[id].tsx
@@ -151,15 +151,83 @@ export default function Video() {
 
   const [subtitles, setSubtitles] = useState<subItems>();
 
+  // Define state variables for the result, recording status, and media recorder
   const [result, setResult] = useState();
   const [recording, setRecording] = useState(false);
-  const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder | null>(null);
-
-  let chunks : BlobPart[] = [];
+  const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder | null>(
+    null
+  );
+  // This array will hold the audio data
+  let chunks: BlobPart[] = [];
+  // This useEffect hook sets up the media recorder when the component mounts
 
   const router = useRouter();
   const { id } = router.query;
   const videoId = id;
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      navigator.mediaDevices
+        .getUserMedia({ audio: true })
+        .then((stream) => {
+          const newMediaRecorder = new MediaRecorder(stream);
+          newMediaRecorder.onstart = () => {
+            chunks = [];
+          };
+          newMediaRecorder.ondataavailable = (e) => {
+            chunks.push(e.data);
+          };
+          newMediaRecorder.onstop = async () => {
+            const audioBlob = new Blob(chunks, { type: "audio/webm" });
+            const audioUrl = URL.createObjectURL(audioBlob);
+            const audio = new Audio(audioUrl);
+            audio.onerror = function (err) {
+              console.error("Error playing audio:", err);
+            };
+            audio.play();
+            try {
+              const reader = new FileReader();
+              reader.readAsDataURL(audioBlob);
+              reader.onloadend = async function () {
+                if (reader.result) {
+                  const base64Audio = (reader.result as string).split(",")[1]; // Remove the data URL prefix
+                  const response = await fetch("/api/speechToText", {
+                    method: "POST",
+                    headers: {
+                      "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({ audio: base64Audio }),
+                  });
+                  console.log("here1");
+                  const data = await response.json();
+                  console.log(data);
+                  console.log("here2");
+                  if (response.status !== 200) {
+                    throw (
+                      data.error ||
+                      new Error(`Request failed with status ${response.status}`)
+                    );
+                  }
+                  setResult(data.message);
+                  setQuestion(data.message);
+                } else {
+                  console.error("Reader result is null");
+                }
+              };
+            } catch (error) {
+              if (error instanceof Error) {
+                console.error(error);
+                alert(error.message);
+              } else {
+                console.error(error);
+              }
+            }
+          };
+          setMediaRecorder(newMediaRecorder);
+        })
+        .catch((err) => console.error("Error accessing microphone:", err));
+    }
+  }, []);
 
   useEffect(() => {
     if (!videoId || Array.isArray(videoId)) {
@@ -192,68 +260,6 @@ export default function Video() {
   };
 
   fetchSubtitles();
-
-  if (typeof window !== 'undefined') {
-    navigator.mediaDevices.getUserMedia({ audio: true })
-      .then(stream => {
-        const newMediaRecorder = new MediaRecorder(stream);
-        newMediaRecorder.onstart = () => {
-            console.log("zoom!")
-          chunks = [];
-        };
-        newMediaRecorder.ondataavailable = e => {
-          chunks.push(e.data);
-        };
-        newMediaRecorder.onstop = async () => {
-            console.log("I have stop")
-          const audioBlob = new Blob(chunks, { type: 'audio/webm' });
-          const audioUrl = URL.createObjectURL(audioBlob);
-          const audio = new Audio(audioUrl);
-          audio.onerror = function (err) {
-            console.error('Error playing audio:', err);
-          };
-          audio.play();
-          try {
-            const reader = new FileReader();
-            reader.readAsDataURL(audioBlob);
-            reader.onloadend = async function () {
-              if(reader.result)
-              {
-                const base64Audio = (reader.result as string).split(',')[1]; // Remove the data URL prefix
-                const response = await fetch("/api/speechToText", {
-                method: "POST",
-                headers: {
-                  'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ audio: base64Audio }),
-              });
-              console.log("here1")
-              const data = await response.json();
-              console.log(data)
-              console.log("here2")
-              if (response.status !== 200) {
-                throw data.error || new Error(`Request failed with status ${response.status}`);
-              }
-              setResult(data.message);
-              }
-              else
-              {
-                console.error('Reader result is null');
-              }
-            }
-          } catch (error) {
-            if (error instanceof Error) {
-              console.error(error);
-              alert(error.message);
-            } else {
-              console.error(error);
-            }
-          }
-        };
-        setMediaRecorder(newMediaRecorder);
-      })
-      .catch(err => console.error('Error accessing microphone:', err));
-  }
 
   const onSubmit = async (e: any) => {
     e.preventDefault();
@@ -324,6 +330,20 @@ export default function Video() {
     player.seekTo(time);
   };
 
+  const startRecording = () => {
+    if (mediaRecorder) {
+      mediaRecorder.start();
+      setRecording(true);
+    }
+  };
+  // Function to stop recording
+  const stopRecording = () => {
+    if (mediaRecorder) {
+      mediaRecorder.stop();
+      setRecording(false);
+    }
+  };
+
   type Interval = {
     start: number;
     end: number;
@@ -344,22 +364,6 @@ export default function Video() {
       console.log("zoom");
     }
   }
-
-  const startRecording = () => {
-    if (mediaRecorder) {
-        console.log("I am on");
-      mediaRecorder.start();
-      setRecording(true);
-    }
-  };
-  // Function to stop recording
-  const stopRecording = () => {
-    if (mediaRecorder) {
-        console.log("I am off");
-      mediaRecorder.stop();
-      setRecording(false);
-    }
-  };
 
   //console.log(intervals);
 
@@ -437,10 +441,11 @@ export default function Video() {
           }}
         ></div>
       </form>
-      <button onClick={recording ? stopRecording : startRecording} >
-          {recording ? 'Stop Recording' : 'Start Recording'}
+      <div className={styles.description}>
+        <button onClick={recording ? stopRecording : startRecording}>
+          {recording ? "Stop Recording" : "Start Recording"}
         </button>
-        <p>{result}</p>
+      </div>
     </main>
   );
 }
