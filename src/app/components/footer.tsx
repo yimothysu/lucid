668372e0814587c544633e71058703a499f99b0c
benny <bennycortese@gmail.com>
2023-10-15 03:15:27 -0400

    cookung

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,16 +1,39 @@
-import Image from "next/image";
-import styles from "./style.module.css";
-import Link from "next/link";
+import { CircleFlag } from 'react-circle-flags'
 export default function Navbar() {
-  const countries = [];
+  const countries = [
+    {
+        countryCode: "us"
+    },
+    {
+        countryCode: "es"
+    },
+    {
+        countryCode: "fr"
+    },
+    {
+        countryCode: "de"
+    },
+    {
+        countryCode: "in"
+    }
+  ];
 
   return (
     <div
       style={{
         backgroundColor: "#f1f1ee",
+        padding: "1em",
+        display: "flex",
+        alignContent: "center",
+        justifyContent: "center",
+        gap: "1em"
       }}
     >
-      Test!
+     {
+        countries.map((item, index) => {
+            return <CircleFlag countryCode={`${item.countryCode}`} height='50px' />
+        })
+     }
     </div>
   );
 }
